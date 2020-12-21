c676984c759a1eed64cacb5383a55e60d0e6f6f0
arskaout <aaron.haarti@gmail.com>
2020-12-21 13:09:44 +0200

    added mandelbrot

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
+from mandelbrot_generate import generate
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    mandelbrot_data = generate()
+    return render_template('main.html',mandelbrot_data=mandelbrot_data)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    data = generate()
+    print(data)
+    
---
+++ mandelbrot_generate.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import mandelbrot
+import matplotlib.pyplot as plt
+import time
+
+
+def generate(min_x=-2,max_x=1,min_y=-1,max_y=1, n=1000,):
+
+    abs_x = abs(max_x-min_x)
+    abs_y = abs(max_y-min_y)
+    norm_x = abs_x/(abs_x+abs_y)
+    norm_y = abs_y/(abs_x+abs_y)
+    i_n = int(norm_x*n)
+    j_n = int(norm_y*n)
+
+    data = mandelbrot.generate_heat_map(min_x,max_y,i_n,j_n,abs_x,abs_y)
+
+
+    return data
+
+if __name__ == '__main__':
+    generate()
